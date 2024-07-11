77f778c44501bfb4390a9b0d87e52f7b56b9fa25
JMSERVICES2021 <jriveros2012@gmail.com>
2024-07-11 19:52:40 -0400

    antes de clases

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    $('#enviarCorreo').on('click', function() {
+        alert("El correo fue enviado correctamente...");
+    });
+
+    $(document).ready(function() {
+        $("#quienessomos").dblclick(function(){
+            $(this).css({
+                "color": "blue"
+            });
+        });
+
+        $("#quienessomos").click(function(){
+            $(this).css({
+                "color": "red",
+                "font-size": "2em"
+            });
+        });
+
+        
+    });
+});
 
 document.addEventListener('DOMContentLoaded', function () {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
@@ -10,44 +37,3 @@ document.addEventListener('DOMContentLoaded', function () {
         alert("El correo fue enviado correctamente...");
     });
 });
-
-    $(document).ready(function(){
-    $("#quienessomos").dblclick(function(){
-        $(this).hide('slow');
-    });
-   
-    $("#quienessomos").click(function(){
-        $(this).css({
-            "color": "red",
-            "font-size": "2em"
-        });
-    });
-   
-    $("#quienessomoss").hover(function(){
-        $(this).html("<a href='#'>Me convertí en un enlace</a>");
-    }, function() {
-        $(this).html("RECETAS RELACIONADAS");
-    });
-});
-
-
-
-          $(document).ready(function() {
-              $("#quienessomos").dblclick(function() {
-                  $(this).hide('slow');
-              });
-  
-              $("#quienessomos").click(function() {
-                  $(this).css({
-                      "color": "red",
-                      "font-size": "2em"
-                  });
-              });
-  
-              $("#quienessomos").hover(function() {
-                  $(this).html("<a href='#'>Me convertí en un enlace</a>");
-              }, function() {
-                  $(this).html("¿QUIENES SOMOS?");
-              });
-          });
-    
