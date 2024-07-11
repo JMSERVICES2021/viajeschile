05d250775c97866198a66c98594b635ea06ebd51
JMSERVICES2021 <jriveros2012@gmail.com>
2024-07-11 16:44:45 -0400

    act

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,33 @@
-function cambiarColorTitulo() {
-    var titulo = document.getElementById('quienessomos');
-    titulo.style.color = '#0CCAF2';
-}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+
+    $('#enviarCorreo').on('click', function() {
+        alert("El correo fue enviado correctamente...");
+    });
+});
+
+    $(document).ready(function(){
+    $("#quienessomos").dblclick(function(){
+        $(this).hide('slow');
+    });
+   
+    $("#quienessomos").click(function(){
+        $(this).css({
+            "color": "red",
+            "font-size": "2em"
+        });
+    });
+   
+    $("#quienessomoss").hover(function(){
+        $(this).html("<a href='#'>Me convertí en un enlace</a>");
+    }, function() {
+        $(this).html("RECETAS RELACIONADAS");
+    });
+});
+
+
